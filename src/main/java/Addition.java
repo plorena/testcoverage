8704f36fdd501258bb8ffc6539e0e4dae7bdb186
Paul LORENA <lorena.paul@telenetgroup.be>
2019-03-30 22:01:31 +0100

    first commit

--- src/main/java/Addition.java
+++ src/main/java/Addition.java
@@ -2,4 +2,17 @@ public class Addition implements Operation {
     public Integer operation(int i, int j) {
         return i + j;
     }
+    public Integer operation2(int i, int j) {
+        return i + j;
+    }
+    public Integer operation3(int i, int j) {
+        return i + j;
+    }
+    public Integer operation4(int i, int j) {
+        return i + j;
+    }
+
+    public Integer operation5(int i, int j) {
+        return i + j;
+    }
 }
--- src/main/java/Division.java
+++ src/main/java/Division.java
@@ -3,4 +3,7 @@ public class Division implements Operation {
     public Integer operation(int i, int j) {
         return i/j;
     }
+    public Integer operation2(int i, int j) {
+        return i/j;
+    }
 }
